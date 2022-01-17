c02c9d420daecbb4c4c5aefc64b1889873989fb7
Adrian Żak <48351330+Zmroqk@users.noreply.github.com>
2022-01-17 13:13:07 +0100

    Terminal Script(#2)
    * Initialized terminal
    
    * basic functions in terminal
    
    * reg. terminal and topics set
    
    * up reg. terminal
    
    Co-authored-by: marcijay <jak.marcinko@gmail.com>
    Co-authored-by: piXel2K <rafal.piwnik@gmail.com>

---
+++ RegistrationTerminal/registration_terminal.py
@@ -0,0 +1,60 @@
+import ssl
+import paho.mqtt.client as mqtt
+
+terminal_id = 'RT0'
+broker = '192.168.56.1'
+
+client = mqtt.Client()
+
+# Send to server on register/<<terminal_id>>
+def send_message(user_id, card_id):
+    """Message payload format: <<user_id>>.<<card_id>>"""
+    client.publish(f'register/{terminal_id}', f'{user_id}.{card_id}')
+
+def interface():
+    should_run = True
+    while should_run:
+        user_id = input('State user id: ')
+        card_id = input('State card id: ')
+        send_message(user_id, card_id)
+
+def process_message(client, userdata, message):
+    """Message payload format: (1) success.<<user_id>>.<<card_id>> (2) failure.<<reason>>"""
+    message_decoded = (str(message.payload.decode("utf-8"))).split(".")
+
+    try:
+        if message_decoded[0].lower() == 'failure':
+            print('Operation denied: ' + message_decoded[1])
+        elif message_decoded[0].lower() == 'success':
+            print(f'Card {message_decoded[1]} successfully added for user {message_decoded[2]}.')
+    except IndexError:
+        print('Illegal server response')
+        
+
+# Receiver from server register/<<terminal_id>>/response
+def connect_to_broker():
+    client.username_pw_set('server', 'ServerPassword')
+    client.tls_set('ca.crt', tls_version=ssl.PROTOCOL_TLSv1_2)
+    client.tls_insecure_set(True)
+
+    client.connect(broker)
+    client.on_message = process_message
+    client.loop_start()
+    client.subscribe(f'register/{terminal_id}/response')
+
+
+def disconnect_from_broker():
+    client.loop_stop()
+    client.disconnect()
+
+def run_sender():
+    interface()
+
+    connect_to_broker()
+
+    disconnect_from_broker()
+
+if __name__ == "__main__":
+    run_sender()
+
+
---
+++ Terminal/terminal.py
@@ -0,0 +1,57 @@
+import paho.mqtt.client as mqtt
+
+terminal_id = 'T0'
+broker = '192.168.56.1'
+
+client = mqtt.Client()
+
+# Send to server on terminal/<<terminal_id>>
+def send_message(card_id):
+    client.publish(f'terminal/{terminal_id}', card_id)
+
+def interface():
+    should_run = True
+    while should_run:
+        card_id = input('State card id: ')
+        send_message(card_id)
+
+def process_message(client, userdata, message):
+    """Message payload format: (1) allow.<<rent_status>> (2) denied.<<reason>>"""
+    message_decoded = (str(message.payload.decode("utf-8"))).split(".")
+
+    try:
+        if message_decoded[0].lower() == 'denied':
+            print('Operation denied: ' + message_decoded[1])
+        elif message_decoded[0].lower() == 'allow':
+            print('Operation allowed: ' + message_decoded[1])
+    except IndexError:
+        print('Illegal server response')
+        
+
+# Receiver from server terminal/<<terminal_id>>/response 
+def connect_to_broker():
+    client.username_pw_set('server', 'ServerPassword')
+    client.tls_set('ca.crt', tls_version=ssl.PROTOCOL_TLSv1_2)
+    client.tls_insecure_set(True)
+
+    client.connect(broker)
+    client.on_message = process_message
+    client.loop_start()
+    client.subscribe(f'terminal/{terminal_id}/response')
+
+
+def disconnect_from_broker():
+    client.loop_stop()
+    client.disconnect()
+
+def run_sender():
+    interface()
+
+    connect_to_broker()
+
+    disconnect_from_broker()
+
+if __name__ == "__main__":
+    run_sender()
+
+
